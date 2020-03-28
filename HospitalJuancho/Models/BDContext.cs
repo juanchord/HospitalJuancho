4a7281ad241533c55d094bc141cc99aec6747137
JuanManuel <litecorp@outlook.com>
2020-03-28 15:43:06 -0400

    Creando Context

---
+++ HospitalJuancho/Models/BDContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace HospitalJuancho.Models
+{
+    public class BDContext:DbContext
+    {
+        public BDContext()
+            :base("HospitalJuancho")
+        { }
+
+        public DbSet<Medicos> Medicos { get; set; }
+        public DbSet<Pacientes> Pacientes { get; set; }
+        public DbSet<Habitaciones> Habitaciones { get; set; }
+        public DbSet<Citas> Citas { get; set; }
+        public DbSet<Ingresos> Ingresos { get; set; }
+        public DbSet<Altas> Altas { get; set; }
+
+
+
+
+
+    }
+}
